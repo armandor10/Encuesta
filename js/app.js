e9df1e7b0ae44b7133308b071e08b602018f83d6
armandor10 <armandorp10@gmail.com>
2015-10-17 01:01:52 -0500

    Adapatacion de la pantalla con google maps

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module("myApp", ['uiGmapgoogle-maps']);
+
+app.config(function(uiGmapGoogleMapApiProvider) {
+ uiGmapGoogleMapApiProvider.configure({
+  key: 'AIzaSyDe9J6-DNDqHvB9zU_icstOH0TtGA4vLcw',
+  v: '3.17',
+  libraries: 'weather,geometry,visualization'
+ });
+});
--- js/controllers/MapsController.js
+++ js/controllers/MapsController.js
@@ -1,13 +1,35 @@
 app.controller("MapsController", function($scope, uiGmapGoogleMapApi) {
   
   // Define variables for our Map object
-  var areaLat      = 44.2126995,
-      areaLng      = -100.2471641,
-      areaZoom     = 3;
+  var areaLat      = 10.461835,
+      areaLng      = -73.253903,
+      areaZoom     = 14;
 
   uiGmapGoogleMapApi.then(function(maps) {
-    $scope.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom };
+    $scope.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom , bounds: {}};
     $scope.options = { scrollwheel: false };
+    
+    $scope.randomMarkers = [
+    	{
+    		id: 0,
+      		coords: {
+		        latitude: 10.461835,
+		        longitude: -73.253904
+      		}
+  		},
+  		{
+    		id: 1,
+      		coords: {
+		        latitude: 10.461835,
+		        longitude: -73.253902
+      		}
+  		}
+    ];
+
   });
+
+
+
+
 
 });
